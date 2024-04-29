73c7fe0d9fa332a960d222554aa050c6632adc77
CHOIJIONE\choij <choijione02@gmail.com>
2024-04-29 18:24:03 +0900

    타일 맵을 표시할 ImGui 레이아웃 추가

--- Transform3D/Src/Main.cpp
+++ Transform3D/Src/Main.cpp
@@ -1,6 +1,8 @@
 #include <cstdint>
 #include <Windows.h>
 
+#include <imgui.h>
+
 #include "CrashModule.h"
 #include "PlatformModule.h"
 #include "RenderModule.h"
@@ -23,6 +25,11 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{		
+			ImGui::Begin("TileMap", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+			ImGui::SetWindowPos(ImVec2(10.0f, 10.0f));
+			ImGui::SetWindowSize(ImVec2(800.0f, 600.0f));
+			ImGui::End();
+			
 			RenderModule::SetWindowViewport();
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 			RenderModule::EndFrame();
