969dace2db7af0f86cbfbc48b2ae93e0799911c9
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-04-30 09:29:01 +0900

    3D 위치와 색상 정보를 가진 정점 추가

--- Module/RenderModule/Inc/Vertex3D.h
+++ Module/RenderModule/Inc/Vertex3D.h
@@ -110,3 +110,137 @@ struct VertexPosition3D
 	 */
 	Vec3f position;
 };
+
+
+/**
+ * @brief 3D 위치와 색상 정보를 가진 정점입니다.
+ */
+struct VertexPositionColor3D
+{
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 기본 생성자입니다.
+	 */
+	VertexPositionColor3D() noexcept
+		: position(0.0f, 0.0f, 0.0f)
+		, color(0.0f, 0.0f, 0.0f, 0.0f) {}
+
+
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param p 정점의 위치입니다.
+	 * @param c 정점의 색상입니다.
+	 */
+	VertexPositionColor3D(Vec3f&& p, Vec4f&& c) noexcept
+		: position(p)
+		, color(c) {}
+
+
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param p 정점의 위치입니다.
+	 * @param c 정점의 색상입니다.
+	 */
+	VertexPositionColor3D(const Vec3f& p, const Vec4f& c) noexcept
+		: position(p)
+		, color(c) {}
+
+
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param x 위치의 X좌표입니다.
+	 * @param y 위치의 Y좌표입니다.
+	 * @param z 위치의 Z좌표입니다.
+	 * @param r 정점 색상의 R입니다.
+	 * @param g 정점 색상의 G입니다.
+	 * @param b 정점 색상의 B입니다.
+	 * @param a 정점 색상의 A입니다.
+	 */
+	VertexPositionColor3D(
+		float x, float y, float z,
+		float r, float g, float b, float a
+	) noexcept
+		: position(x, y, z)
+		, color(r, g, b, a) {}
+
+
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionColor3D(VertexPositionColor3D&& instance) noexcept
+		: position(instance.position)
+		, color(instance.color) {}
+
+
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 생성자입니다.
+	 *
+	 * @param instance 복사할 정점 인스턴스입니다.
+	 */
+	VertexPositionColor3D(const VertexPositionColor3D& instance) noexcept
+		: position(instance.position)
+		, color(instance.color) {}
+
+
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionColor3D& operator=(VertexPositionColor3D&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position = instance.position;
+		color = instance.color;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 3D 위치와 색상 정보를 가진 정점의 대입 연산자입니다.
+	 *
+	 * @param instance 대입할 정점 인스턴스입니다.
+	 *
+	 * @return 대입한 객체의 참조자를 반환합니다.
+	 */
+	VertexPositionColor3D& operator=(const VertexPositionColor3D& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		position = instance.position;
+		color = instance.color;
+
+		return *this;
+	}
+
+
+	/**
+	 * @brief 정점의 바이트 보폭 값을 얻습니다.
+	 *
+	 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
+	 */
+	static uint32_t GetStride()
+	{
+		return sizeof(VertexPositionColor3D);
+	}
+
+
+	/**
+	 * @brief 정점의 위치입니다.
+	 */
+	Vec3f position;
+
+
+	/**
+	 * @brief 정점의 색상입니다.
+	 */
+	Vec4f color;
+};
