3933623e4ccc33ffd1b2216342eff25c0ea9899c
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-01 16:48:32 +0900

    격자 및 와이어프레임 큐브를 그리는 테스트 코드 추가

--- Transform3D/Src/Main.cpp
+++ Transform3D/Src/Main.cpp
@@ -11,6 +11,7 @@
 #include "TileMap.h"
 #include "Vertex3D.h"
 #include "GeometryGenerator.h"
+#include "GeometryRenderer3D.h"
 #include "StaticMesh.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
@@ -22,125 +23,51 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 
 	HWND hwnd = PlatformModule::GetWindowHandle();
 	RenderModule::Init(hwnd);
-
+	
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
 	RenderModule::SetPointSizeMode(true);
 	RenderModule::SetAlphaBlendMode(true);
 	RenderModule::SetDepthMode(true);
 
-	//std::vector<VertexPositionUv3D> vertices;
-	//std::vector<uint32_t> indices;
-	//GeometryGenerator::CreateCube<VertexPositionUv3D>(Vec3f(2.0f, 2.0f, 2.0f), vertices, indices);
+	std::vector<VertexPositionNormalUv3D> vertices;
+	std::vector<uint32_t> indices;
+	GeometryGenerator::CreateCube(Vec3f(2.0f, 2.0f, 2.0f), vertices, indices);
 
-	//StaticMesh<VertexPositionUv3D>* mesh = RenderModule::CreateResource<StaticMesh<VertexPositionUv3D>>(vertices, indices);
+	StaticMesh<VertexPositionNormalUv3D>* mesh = RenderModule::CreateResource<StaticMesh<VertexPositionNormalUv3D>>(vertices, indices);
 	Shader* shader = RenderModule::CreateResource<Shader>("Resource/Shader/Shader.vert", "Resource/Shader/Shader.frag");
+	GeometryRenderer3D* renderer = RenderModule::CreateResource< GeometryRenderer3D>();
 	TileMap* tileMap = RenderModule::CreateResource<TileMap>(TileMap::ESize::Size_512x512, TileMap::ESize::Size_128x128, Vec4f(1.0f, 0.0f, 0.0f, 1.0f), Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
 
 	int32_t screenWidth = 0;
 	int32_t screenHeight = 0;
 	RenderModule::GetScreenSize(screenWidth, screenHeight);
-
-	float vertices[] = {
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	float aspect = static_cast<float>(screenWidth) / static_cast<float>(screenHeight);
 	
+	Vec3f extensions = Vec3f(1.0f, 1.0f, 1.0f);
+	Vec4f color = Vec4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+	renderer->SetView(Mat4x4::LookAt(Vec3f(10.0f, 10.0f, 10.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f)));
+	renderer->SetProjection(Mat4x4::Perspective(MathModule::ToRadian(45.0f), aspect, 0.01f, 100.0f));
+
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{		
 			ImGui::Begin("TileMap", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
 			ImGui::SetWindowPos(ImVec2(10.0f, 10.0f));
-			ImGui::SetWindowSize(ImVec2(200.0f, 200.0f));
-
-			float windowWidth = ImGui::GetContentRegionAvail().x;
-			float windowHeight = ImGui::GetContentRegionAvail().y;
-
-			ImGui::Image((void*)(intptr_t)(tileMap->GetID()), ImVec2(windowWidth, windowHeight), ImVec2(0, 0), ImVec2(1, 1));
+			ImGui::SetWindowSize(ImVec2(400.0f, 100.0f));
+			ImGui::SliderFloat3("extensions", extensions.data, 0.0f, 3.0f);
+			ImGui::ColorEdit4("color", color.data);
 			ImGui::End();
 			
 			RenderModule::SetWindowViewport();
-			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+			RenderModule::BeginFrame(0.3f, 0.3f, 0.3f, 1.0f);
 
 			static float time = 0.0f;
 			time += deltaSeconds;
 
-			shader->Bind();
-			{
-				tileMap->Active(0);
-				shader->SetUniform("model", Mat4x4::Scale(2.0f, 2.0f, 2.0f) * Mat4x4::RotateY(time));
-				shader->SetUniform("view", Mat4x4::LookAt(Vec3f(10.0f, 10.0f, 10.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f)));
-				shader->SetUniform("projection", Mat4x4::Perspective(
-					MathModule::ToRadian(45.0f), 
-					static_cast<float>(screenWidth) / static_cast<float>(screenHeight),
-					0.01f,
-					100.0f
-				));
-
-				glBindVertexArray(VAO);
-				glDrawArrays(GL_TRIANGLES, 0, 36);
-				glBindVertexArray(0);
-
-				//mesh->Bind();
-				//glDrawElements(GL_TRIANGLES, mesh->GetIndexCount(), GL_UNSIGNED_INT, nullptr);
-				//mesh->Unbind();
-			}
-			shader->Unbind();
-
-
+			renderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
+			renderer->DrawCube3D(Mat4x4::RotateZ(time), extensions, color);
+			
 			RenderModule::EndFrame();
 		}
 	);
