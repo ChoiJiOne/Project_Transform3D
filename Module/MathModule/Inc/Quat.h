5fb3358fc0cd75f57e89d325638f0a3de66222f7
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-02 15:08:23 +0900

    쿼터니언 추가

---
+++ Module/MathModule/Inc/Quat.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Mat4x4.h"
+#include "MathModule.h"
+#include "Vec3.h"
+
+
+/**
+ * @brief 쿼터니언입니다.
+ * 
+ * @see 
+ * - https://gabormakesgames.com/quaternions.html
+ * - https://en.wikipedia.org/wiki/Quaternion
+ */
+struct Quat
+{
+	/**
+	 * @brief 쿼터니언의 다양한 원소 형식입니다.
+	 */
+	union
+	{
+		struct
+		{
+			float x;
+			float y;
+			float z;
+			float w;
+		};
+		struct 
+		{
+			Vec3f v;
+			float w;
+		};
+		float data[4];
+	};
+};
