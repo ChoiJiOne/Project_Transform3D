f1af9d5622da56418d19ac37d566d1e568b26850
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-03 19:02:18 +0900

    구 엔티티 추가

---
+++ Transform3D/Inc/Sphere.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <imgui.h>
+
+#include "Transform.h"
+#include "StaticMesh.h"
+#include "TileMap.h"
+#include "Vertex3D.h"
+
+#include "IEntity.h"
+
+class MeshRenderer;
+
+
+/**
+ * @brief 구(Sphere) 엔티티입니다.
+ */
+class Sphere : public IEntity
+{
+public:
+	/**
+	 * @brief 구(Sphere) 엔티티의 생성자입니다.
+	 *
+	 * @param renderer 구를 렌더링할 때 참조할 렌더러입니다.
+	 */
+	Sphere(MeshRenderer* renderer);
+
+
+	/**
+	 * @brief 구(Sphere) 엔티티의 가상 소멸자입니다.
+	 */
+	virtual ~Sphere();
+
+
+	/**
+	 * @brief 구(Sphere) 엔티티의 복사 생성자와 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Sphere);
+
+
+	/**
+	 * @brief 구(Sphere) 엔티티를 업데이트합니다.
+	 *
+	 * @param deltaSeconds 델타 시간 값입니다.
+	 */
+	virtual void Tick(float deltaSeconds);
+
+
+	/**
+	 * @brief 구(Sphere) 엔티티를 화면에 그립니다.
+	 */
+	virtual void Render() override;
+
+
+	/**
+	 * @brief 구(Sphere) 엔티티 내의 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+private:
+	/**
+	 * @brief ImGui 윈도우 속성입니다.
+	 */
+	ImGuiWindowFlags_ windowFlags_;
+
+
+	/**
+	 * @brief ImGui 내의 위치입니다.
+	 */
+	ImVec2 location_;
+
+
+	/**
+	 * @brief ImGui 내의 크기입니다.
+	 */
+	ImVec2 size_;
+
+
+	/**
+	 * @brief 구의 메시 리소스입니다.
+	 */
+	StaticMesh<VertexPositionNormalUv3D>* mesh_ = nullptr;
+
+
+	/**
+	 * @brief 구에 매핑할 타일맵입니다.
+	 */
+	TileMap* tileMap_ = nullptr;
+
+
+	/**
+	 * @brief 구의 월드 상 위치입니다.
+	 */
+	Vec3f position_ = Vec3f(0.0f, 0.0f, 0.0f);
+
+
+	/**
+	 * @brief 구의 회전 축입니다.
+	 */
+	Vec3f axis_ = Vec3f(0.0f, 0.0f, 0.0f);
+	
+	
+	/**
+	 * @brief 회전 축을 중심으로 회전할 라디안 각도입니다.
+	 */
+	float radian_ = 0.0f;
+
+
+	/**
+	 * @brief 구의 크기입니다.
+	 */
+	Vec3f scale_ = Vec3f(1.0f, 1.0f, 1.0f);
+
+
+	/**
+	 * @brief 구의 트랜스폼입니다.
+	 */
+	Transform transform_;
+
+
+	/**
+	 * @brief 구 렌더링 시 참조할 렌더러입니다.
+	 */
+	MeshRenderer* renderer_ = nullptr;
+};
---
+++ Transform3D/Src/Sphere.cpp
@@ -0,0 +1,95 @@
+#include "GeometryGenerator.h"
+#include "RenderModule.h"
+#include "MeshRenderer.h"
+
+#include "Sphere.h"
+
+Sphere::Sphere(MeshRenderer* renderer)
+	: windowFlags_(static_cast<ImGuiWindowFlags_>(ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
+{
+	location_ = ImVec2(10.0f, 10.0f);
+	size_ = ImVec2(400.0f, 200.0f);
+
+	std::vector<VertexPositionNormalUv3D> vertices;
+	std::vector<uint32_t> indices;
+	GeometryGenerator::CreateSphere(1.0f, 40, vertices, indices);
+	mesh_ = RenderModule::CreateResource<StaticMesh<VertexPositionNormalUv3D>>(vertices, indices);
+
+	tileMap_ = RenderModule::CreateResource<TileMap>(TileMap::ESize::Size_512x512, TileMap::ESize::Size_64x64, Vec4f(1.0f, 0.0f, 0.0f, 1.0f), Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+}
+
+Sphere::~Sphere()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Sphere::Tick(float deltaSeconds)
+{
+	ImGui::Begin("Transform", nullptr, windowFlags_);
+	ImGui::SetWindowPos(location_);
+	ImGui::SetWindowSize(size_);
+
+	ImGui::Text("Position");
+	ImGui::Text("position");
+	ImGui::SameLine();
+	ImGui::SliderFloat3("##position", position_.data, -10.0f, 10.0f);
+	ImGui::SameLine();
+	if (ImGui::Button("Reset##position"))
+	{
+		position_ = Vec3f(0.0f, 0.0f, 0.0f);
+	}
+	ImGui::Separator();
+
+	ImGui::Text("Rotate");
+	ImGui::Text("%-6s", "axis");
+	ImGui::SameLine();
+	ImGui::SliderFloat3("##axis", axis_.data, -1.0f, 1.0f);
+	ImGui::SameLine();
+	if (ImGui::Button("Reset##axis"))
+	{
+		axis_ = Vec3f(0.0f, 0.0f, 0.0f);
+	}
+
+	ImGui::Text("%-6s", "radian");
+	ImGui::SameLine();
+	ImGui::SliderFloat("##radian", &radian_, -TwoPi, TwoPi);
+	ImGui::SameLine();
+	if (ImGui::Button("Reset##radian"))
+	{
+		radian_ = 0.0f;
+	}
+
+	ImGui::Separator();
+
+	ImGui::Text("Scale");
+	ImGui::Text("scale");
+	ImGui::SameLine();
+	ImGui::SliderFloat3("##scale", scale_.data, 0.0f, 10.0f);
+	ImGui::SameLine();
+	if (ImGui::Button("Reset##scale"))
+	{
+		scale_ = Vec3f(1.0f, 1.0f, 1.0f);
+	}
+
+	ImGui::End();
+
+	transform_.position = position_;
+	transform_.rotate = Quat::AxisRadian(axis_, radian_);
+	transform_.scale = scale_;
+}
+
+void Sphere::Render()
+{
+	renderer_->DrawMesh(mesh_, Transform::ToMat(transform_), tileMap_);
+}
+
+void Sphere::Release()
+{
+	if (bIsInitialized_)
+	{
+		bIsInitialized_ = false;
+	}
+}
